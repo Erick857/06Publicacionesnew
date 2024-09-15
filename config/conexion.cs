f6758be33a34451d4a2b01fc5f103f81e1e08fb0
Asus <Asus@DESKTOP-BUUJ9LF>
2024-09-15 17:13:12 -0500

    Agregar archivos de proyecto.

---
+++ config/conexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _06Publicaciones.config
+{
+    public static class Conexion
+    {
+        private static readonly string connectionString;
+
+        static Conexion()
+        {
+            // Corregido el nombre de la instancia
+            connectionString = @"Server=DESKTOP-BUUJ9LF\SQLEXPRESS01;Database=pubs;User Id=sa;Password=erick;";
+        }
+
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                var connection = new SqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"Error al conectar con la base de datos: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
